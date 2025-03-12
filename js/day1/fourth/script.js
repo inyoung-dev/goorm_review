9b8b73ac3505658c10793f41b69eaf1c7445da54
zero <inzero1998@gmail.com>
2025-03-12 15:06:22 +0900

    Feat: js day1 seventh까지 완료

---
+++ js/day1/fourth/script.js
@@ -0,0 +1,11 @@
+/*
+
+1. 다음 코드의 결과로 무엇이 출력될까요?
+console.log(null || 2 || undefined);
+
+2. 다음 코드의 결과로 무엇이 출력될까요?
+console.log(1 && null && 3);
+
+1. || (or 연산자) truthy인 2가 출력!
+2. && (and 연산자) 첫번째 falsy인 null이 출력!ㄴ
+*/
